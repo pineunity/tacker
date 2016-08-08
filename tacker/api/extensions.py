0fe85d6aa0bd4e0cc3cd390e22b81c99ec73478a
doantungbk <doantungbk.203@gmail.com>
2016-08-08 02:06:36 -0700

    Update extensions

--- tacker/api/extensions.py
+++ tacker/api/extensions.py
@@ -277,12 +277,14 @@ class ExtensionMiddleware(wsgi.Middleware):
                                "/%s/{%s_id}" %
                                (resource.parent["collection_name"],
                                 resource.parent["member_name"]))
-
+                LOG.debug(_('tung resource: %s'),            # I'm here
+                      path_prefix)
             LOG.debug(_('Extended resource: %s'),
                       resource.collection)
             for action, method in six.iteritems(resource.collection_actions):
                 conditions = dict(method=[method])
                 path = "/%s/%s" % (resource.collection, action)
+                LOG.debug(_('tung path: %s'), path)           # I'm here
                 with mapper.submapper(controller=resource.controller,
                                       action=action,
                                       path_prefix=path_prefix,
@@ -364,7 +366,7 @@ class ExtensionMiddleware(wsgi.Middleware):
     @webob.dec.wsgify(RequestClass=wsgi.Request)
     def __call__(self, req):
         """Route the incoming request with router."""
-        LOG.debug(_('tung request: %s'), req.url)
+        LOG.debug(_('tung request: %s'), req.url)                  # I'm here
         req.environ['extended.app'] = self.application
         return self._router
 
