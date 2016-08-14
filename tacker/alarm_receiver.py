22244113d6289383678d0651724a06e610ba88cf
pineunity <tungdoan@dcn.ssu.ac.kr>
2016-08-14 15:59:59 +0900

    Test-hope

--- tacker/alarm_receiver.py
+++ tacker/alarm_receiver.py
@@ -13,7 +13,7 @@
 #    under the License.
 
 import logging
-from six.moves.urllib import parse as urlparse
+#from six.moves.urllib import parse as urlparse
 from tacker import wsgi
 import webob.dec
 # from tacker.vm.monitor_drivers.token import Token
@@ -29,22 +29,23 @@ class AlarmReceiver(wsgi.Middleware):
         #    return
         url = req.url
         LOG.debug(_('hll: %s'), url)
-        device_id, params = self.handle_url(req.url)
-        LOG.debug(_('dvc: %s'), device_id)
+#        device_id, params = self.handle_url(req.url)
+#        LOG.debug(_('dvc: %s'), device_id)
         self.validate_url(url)
         return self.application
 
-    def handle_url(self, url):
-        parts = urlparse.urlparse(url)
-        p = parts.path.split('/')
-        LOG.debug(_('Alarm url triggered: %s'), url)
-        if len(p) != 6:
-            return None
-        if any((p[0] != '', p[2] != 'vnfs')):
-            return None
-        qs = urlparse.parse_qs(parts.query)
-        params = dict((k, v[0]) for k, v in qs.items())
-        return p[3], params
+#    def handle_url(self, url):
+#        parts = urlparse.urlparse(url)
+#        p = parts.path.split('/')
+#        LOG.debug(_('Alarm url triggered: %s'), url)
+#        if len(p) != 6:
+#            return None
+#        if any((p[0] != '', p[2] != 'vnfs')):
+#            return None
+#        qs = urlparse.parse_qs(parts.query)
+#        params = dict((k, v[0]) for k, v in qs.items())
+#        return p[3], params
 
     def validate_url(self, url):
+        LOG.debug(_('Alarm url triggered: %s'), url)
         return True
