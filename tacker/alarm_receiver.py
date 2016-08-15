e190af3a87a93deae4bf1316372c24e02e9ad91a
pineunity <tungdoan@dcn.ssu.ac.kr>
2016-08-15 17:35:10 +0900

    Test hope

--- tacker/alarm_receiver.py
+++ tacker/alarm_receiver.py
@@ -12,7 +12,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 from oslo_serialization import jsonutils
-import copy
 import logging
 from six.moves.urllib import parse as urlparse
 from tacker import wsgi
